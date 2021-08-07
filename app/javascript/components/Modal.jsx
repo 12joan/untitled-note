335bd952828c50c47ff10bc7d176cd6fdeb92b5b
Joe Anderson <joe@osbert.me>
2021-08-07 12:47:53 +0100

    Minor adjustment to modal style

--- app/javascript/components/Modal.jsx
+++ app/javascript/components/Modal.jsx
@@ -21,8 +21,8 @@ const Modal = forwardRef((props, ref) => {
     <div ref={modalEl} id={props.id} className="modal fade" tabIndex="-1">
       <div className="modal-dialog modal-lg modal-dialog-centered">
         <div className="modal-content border-0 rounded-lg p-lg">
-          <div className="modal-header border-0 p-0">
-            <h1 className="modal-title">
+          <div className="modal-header border-0 p-0 mb-2">
+            <h1 className="modal-title h2">
               {props.title}
             </h1>
 
@@ -33,7 +33,7 @@ const Modal = forwardRef((props, ref) => {
               aria-label="Close" />
           </div>
 
-          <div className="modal-body p-0 pt-3 mb-n3">
+          <div className="modal-body p-0 mb-n3">
             {props.children}
           </div>
         </div>
