e01fab1417941c9b4df72b1d995d52804d56229b
Joe Anderson <joe@osbert.me>
2021-10-03 19:11:02 +0100

    Require non-title aliases to be non-blank

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -1,3 +1,5 @@
 class Alias < ApplicationRecord
   belongs_to :document
+
+  validates :text, presence: true, unless: :title?
 end
--- test/models/alias_test.rb
+++ test/models/alias_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class AliasTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'can be blank if title' do
+    assert_predicate create(:alias, text: '', title: true), :persisted?
+  end
+
+  test 'cannot be blank if not title' do
+    assert_raises(ActiveRecord::RecordInvalid) { create(:alias, text: '', title: false) }
+  end
 end
