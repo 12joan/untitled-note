2bafc9c69b7642203a6b2cd9f861d9e884fbe9fd
Joe Anderson <joe@mousetrapped.co.uk>
2022-09-14 08:01:12 +0100

    Refactor sidebar into components

--- app/javascript/components/layout/Sidebar.jsx
+++ app/javascript/components/layout/Sidebar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 import { OverviewLink } from '~/lib/routes'
 
@@ -11,63 +10,74 @@ const Sidebar = () => {
   return (
     <div className="w-full max-w-48 space-y-5 pb-3">
       <section className="-ml-3">
-        {[
-          ['Overview', OverviewIcon],
-          ['New document', NewDocumentIcon],
-          ['Search', SearchIcon],
-        ].map(([label, Icon], index) => (
-          <OverviewLink key={index} className="btn btn-transparent w-full px-3 py-2 flex gap-2 items-center">
-            <span className="text-primary-500 dark:text-primary-400 window-inactive:text-slate-500 dark:window-inactive:text-slate-400">
-              <Icon size="1.25em" noAriaLabel />
-            </span>
-
-            {label}
-          </OverviewLink>
-        ))}
+        <ButtonWithIcon as={OverviewLink} nav icon={OverviewIcon} label="Overview" />
+        <ButtonWithIcon icon={NewDocumentIcon} label="New document" />
+        <ButtonWithIcon icon={SearchIcon} label="Search" />
       </section>
 
-      <section>
-        <strong className="text-slate-500 text-xs uppercase tracking-wide select-none dark:text-slate-400">
-          Pinned documents
-        </strong>
+      <SectionWithHeading heading="Pinned documents">
+        <Button label="Document 1" />
+        <Button label="Document 2" />
+        <Button label="Document 3" />
+      </SectionWithHeading>
 
-        <div className="-ml-3">
-          {['Document 1', 'Document 2', 'Document 3'].map((label, index) => (
-            <button key={index} className="btn btn-transparent w-full px-3 py-1 flex gap-2 items-center">
-              {label}
-            </button>
-          ))}
-        </div>
-      </section>
+      <SectionWithHeading heading="Recently viewed">
+        <Button label="Document 4" />
+        <Button label="Document 5" />
+        <Button label="Document 6" />
+      </SectionWithHeading>
 
-      <section>
-        <strong className="text-slate-500 text-xs uppercase tracking-wide select-none dark:text-slate-400">
-          Recently viewed
-        </strong>
-
-        <div className="-ml-3">
-          {['Document 4', 'Document 5', 'Document 6'].map((label, index) => (
-            <button key={index} className="btn btn-transparent w-full px-3 py-1 flex gap-2 items-center">
-              {label}
-            </button>
-          ))}
-        </div>
-      </section>
-
-      <section>
-        <strong className="text-slate-500 text-xs uppercase tracking-wide select-none dark:text-slate-400">
-          Tags
-        </strong>
-
-        <div className="-ml-3">
-          {['Tag 1', 'Tag 2', 'Tag 3'].map((label, index) => (
-            <button key={index} className="btn btn-transparent w-full px-3 py-1 flex gap-2 items-center">
-              {label}
-            </button>
-          ))}
-        </div>
-      </section>
+      <SectionWithHeading heading="Tags">
+        <Button label="Tag 1" />
+        <Button label="Tag 2" />
+        <Button label="Tag 3" />
+      </SectionWithHeading>
     </div>
+  )
+}
+
+const SectionWithHeading = ({ heading, children }) => {
+  return (
+    <section>
+      <strong className="text-slate-500 text-xs uppercase tracking-wide select-none dark:text-slate-400">
+        {heading}
+      </strong>
+
+      <div className="-ml-3">
+        {children}
+      </div>
+    </section>
+  )
+}
+
+const ButtonWithIcon = ({ as: Component = 'button', icon: Icon, label, ...otherProps }) => {
+  const buttonProps = Component === 'button' ? { type: 'button' } : {}
+
+  return (
+    <Component
+      {...buttonProps}
+      className="btn btn-transparent w-full px-3 py-2 flex gap-2 items-center"
+      {...otherProps}
+    >
+      <span className="text-primary-500 dark:text-primary-400 window-inactive:text-slate-500 dark:window-inactive:text-slate-400">
+        <Icon size="1.25em" noAriaLabel />
+      </span>
+
+      {label}
+    </Component>
+  )
+}
+
+const Button = ({ as: Component = 'button', label, ...otherProps }) => {
+  const buttonProps = Component === 'button' ? { type: 'button' } : {}
+
+  return (
+    <Component
+      {...buttonProps}
+      className="btn btn-transparent w-full px-3 py-1 flex"
+      children={label}
+      {...otherProps}
+    />
   )
 }
 
