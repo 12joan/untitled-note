41c07cb912e2cece43553dde355254cafa024ce9
Joe Anderson <joe@mousetrapped.co.uk>
2022-09-23 21:57:22 +0100

    Fix tooltips not working

--- app/javascript/components/Tippy.jsx
+++ app/javascript/components/Tippy.jsx
@@ -1,14 +1,17 @@
-import React, { forwardRef} from 'react'
-import UpstreamTippy from '@tippyjs/react/headless'
+import React, { forwardRef } from 'react'
+import NormalTippy from '@tippyjs/react'
+import HeadlessTippy from '@tippyjs/react/headless'
 import 'tippy.js/dist/tippy.css'
 
 import { useContext } from '~/lib/context'
 
-const Tippy = forwardRef(({ ...otherProps }, ref) => {
+const Tippy = forwardRef(({ headless = true, ...otherProps }, ref) => {
   const { inModal = false } = useContext()
 
+  const TippyComponent = headless ? HeadlessTippy : NormalTippy
+
   return (
-    <UpstreamTippy
+    <TippyComponent
       ref={ref}
       zIndex={inModal ? 40 : 20}
       {...otherProps}
--- app/javascript/components/Tooltip.jsx
+++ app/javascript/components/Tooltip.jsx
@@ -5,6 +5,7 @@ import Tippy from '~/components/Tippy'
 const Tooltip = ({ ...otherProps }) => {
   return (
     <Tippy
+      headless={false}
       theme="custom"
       arrow={false}
       {...otherProps}
