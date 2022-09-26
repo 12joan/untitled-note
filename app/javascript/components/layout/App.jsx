cc020792d363e99d36aaf8c7965d8b780bd1c30f
Joe Anderson <joe@mousetrapped.co.uk>
2022-09-26 17:05:23 +0100

    Add error boundary at top level of app

--- app/javascript/components/layout/App.jsx
+++ app/javascript/components/layout/App.jsx
@@ -3,16 +3,26 @@ import React from 'react'
 import useTitle from '~/lib/useTitle'
 import { routesComponent } from '~/lib/routes'
 
+import ErrorBoundary from '~/components/ErrorBoundary'
 import ToastContainer from '~/components/layout/ToastContainer'
 
 const App = () => {
   useTitle('Note App')
 
+  const fallback = (
+    <div className="p-5 space-y-3">
+      <h1 className="text-3xl font-medium">An internal error has occurred</h1>
+      {/* TODO: Add contact details */}
+      <p className="text-lg font-light">This probably isn't your fault. Let us know if the problem persists.</p>
+      <p><a href="/" className="btn btn-link font-medium">Go back to the home page</a></p>
+    </div>
+  )
+
   return (
-    <>
+    <ErrorBoundary fallback={fallback}>
       {routesComponent}
       <ToastContainer />
-    </>
+    </ErrorBoundary>
   )
 }
 
