88af51bc5ba36b5d1393f1ab2d5bd9e4ce158a21
Joe Anderson <joe@mousetrapped.co.uk>
2022-09-29 11:42:30 +0100

    Sort tags by document count

--- app/apis/tags_api.rb
+++ app/apis/tags_api.rb
@@ -2,7 +2,7 @@ class TagsAPI < ApplicationAPI
   def index
     set_project
 
-    @project.tags.all.map do |tag|
+    @project.tags.order(documents_count: :desc).all.map do |tag|
       tag.query(params[:query])
     end
   end
