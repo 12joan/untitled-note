b37ab6ae6e659586b93256caa0825d36195216e7
Joe Anderson <joe@mousetrapped.co.uk>
2022-10-18 15:04:04 +0100

    Fix active project indicator not showing

--- app/javascript/components/layout/ProjectsBar.jsx
+++ app/javascript/components/layout/ProjectsBar.jsx
@@ -13,40 +13,42 @@ const ProjectsBar = forwardRef(({ onButtonClick = () => {}, ...otherProps }, ref
   const openNewProjectModal = useNewProject()
 
   return (
-    <div className="space-y-3 p-3">
+    <div className="space-y-3 p-3 pl-0">
       {projects.map(project => {
         const isCurrentProject = project.id == projectId
 
         const hasImage = !!project.image_url
 
         return (
-          <Tooltip key={project.id} content={project.name} placement="right" fixed>
-            <ProjectLink
-              projectId={project.id}
+          <div key={project.id} className="flex gap-2">
+            <div
+              aria-hidden="true"
+              className="opacity-0 data-active:opacity-100 -ml-1 my-2 w-2 h-8 rounded-full bg-primary-500 dark:bg-primary-400 window-inactive:bg-slate-500 dark:window-inactive:bg-slate-400"
               data-active={isCurrentProject}
-              className="w-12 btn aspect-square flex items-center justify-center p-1 relative shadow bg-white dark:bg-slate-800"
-              style={{
-                '--bg-url': hasImage ? `url(${project.image_url})` : undefined
-              }}
-              data-has-bg={hasImage}
-              onClick={onButtonClick}
-              aria-label={project.name}
-              aria-current={isCurrentProject ? 'page' : undefined}
-            >
-              {!hasImage && (
-                <span
-                  aria-hidden="true"
-                  className="font-bold text-xl text-slate-500 dark:text-slate-400"
-                  children={abbreviate(project.name, 1)}
-                />
-              )}
+            />
 
-              <div
-                aria-hidden="true"
-                className="hidden data-active:block absolute inset-y-2 -left-3 -translate-x-1/2 rounded-full w-2 bg-primary-500 dark:bg-primary-400 window-inactive:bg-slate-500 dark:window-inactive:bg-slate-400"
-              />
-            </ProjectLink>
-          </Tooltip>
+            <Tooltip content={project.name} placement="right" fixed>
+              <ProjectLink
+                projectId={project.id}
+                className="w-12 h-12 btn flex items-center justify-center p-1 shadow bg-white dark:bg-slate-800"
+                style={{
+                  '--bg-url': hasImage ? `url(${project.image_url})` : undefined
+                }}
+                data-has-bg={hasImage}
+                onClick={onButtonClick}
+                aria-label={project.name}
+                aria-current={isCurrentProject ? 'page' : undefined}
+              >
+                {!hasImage && (
+                  <span
+                    aria-hidden="true"
+                    className="font-bold text-xl text-slate-500 dark:text-slate-400"
+                    children={abbreviate(project.name, 1)}
+                  />
+                )}
+              </ProjectLink>
+            </Tooltip>
+          </div>
         )
       })}
 
