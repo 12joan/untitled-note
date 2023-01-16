2313e26804fea95c96308772ccf729730fed2c81
Joe Anderson <joe@mousetrapped.co.uk>
2023-01-16 19:15:00 +0000

    Improve the behaviour of deleteForward

--- app/javascript/lib/editor/attachments/withAttachments.js
+++ app/javascript/lib/editor/attachments/withAttachments.js
@@ -6,34 +6,44 @@ import { Editor, Range } from 'slate'
 
 import { nodeAtPathIsEmptyParagraph } from './utils'
 
-const withAttachments = editor => {
-  const { deleteBackward } = editor
+/* If all of the following are true:
+ * - the unit is 'character' or 'word'
+ * - the selection is collapsed
+ * - the selection is at an empty paragraph
+ * - the adjacent block is an attachment
+ * Then delete the paragraph and select the attachment
+ */
 
-  // If all of the following are true:
-  // - the unit is 'character' or 'word'
-  // - the selection is collapsed
-  // - the selection is at an empty paragraph
-  // - the previous block is an attachment
-  // Then delete the paragraph and select the attachment
-  editor.deleteBackward = unit => {
+const withAttachments = editor => {
+  const { deleteBackward, deleteForward } = editor
+
+  const customDeleteBehaviour = (defaultDelete, deltaPath) => unit => {
     if (unit === 'character' || unit === 'word') {
       const { selection } = editor
       const path = selection?.anchor?.path?.slice(0, 1)
 
       if (selection && Range.isCollapsed(selection) && nodeAtPathIsEmptyParagraph(editor, path)) {
-        const previousBlockPath = [path[0] - 1]
-        const previousBlock = Editor.node(editor, previousBlockPath)
+        const adjacentBlockPath = [path[0] + deltaPath]
+        const adjacentBlock = Editor.node(editor, adjacentBlockPath)
 
-        if (previousBlock[0].type === 'attachment') {
+        if (adjacentBlock[0].type === 'attachment') {
           removeNodes(editor, { at: path })
-          select(editor, previousBlockPath)
+
+          // If the node we removed was above the attachment, then the
+          // attachment path will have decreased by 1
+          const newAdjacentBlockPath = [adjacentBlockPath[0] + (deltaPath === 1 ? -1 : 0)]
+          select(editor, newAdjacentBlockPath)
+
           return
         }
       }
     }
 
-    deleteBackward(unit)
+    defaultDelete(unit)
   }
+
+  editor.deleteBackward = customDeleteBehaviour(deleteBackward, -1)
+  editor.deleteForward = customDeleteBehaviour(deleteForward, 1)
 
   return editor
 }
