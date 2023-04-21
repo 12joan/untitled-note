f10df316eb6ab228bb7e07366f006e9a10a8da46
Joe Anderson <joe@mousetrapped.co.uk>
2023-04-21 09:49:15 +0100

    lint BackButton

--- client/components/BackButton.tsx
+++ client/components/BackButton.tsx
@@ -1,55 +1,49 @@
-import React, { useRef } from 'react'
-import { useLocation } from 'react-router-dom'
-
-import { useGlobalKeyboardShortcut } from '~/lib/useGlobalKeyboardShortcut'
-import { OverviewLink } from '~/lib/routes'
-
-import CaretLeftIcon from '~/components/icons/CaretLeftIcon'
+import React, { useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import { OverviewLink } from '~/lib/routes';
+import { useGlobalKeyboardShortcut } from '~/lib/useGlobalKeyboardShortcut';
+import CaretLeftIcon from '~/components/icons/CaretLeftIcon';
 
 export interface BackButtonProps extends Record<string, any> {
-  className?: string
+  className?: string;
 }
 
 export const BackButton = ({
   className: userClassName = '',
   ...otherProps
 }: BackButtonProps) => {
-  const linkRef = useRef<any>()
+  const linkRef = useRef<any>();
 
-  const { state } = useLocation()
-  const { linkOriginator = undefined } = state || {}
+  const { state } = useLocation();
+  const { linkOriginator = undefined } = state || {};
 
-  const label = linkOriginator ?? 'Overview'
+  const label = linkOriginator ?? 'Overview';
 
   const linkProps = {
     ref: linkRef,
     ...otherProps,
     className: `btn btn-link flex items-center gap-1 font-medium ${userClassName}`,
-    children: <>
-      <CaretLeftIcon noAriaLabel />
-      {label}
-    </>,
-  }
+    children: (
+      <>
+        <CaretLeftIcon noAriaLabel />
+        {label}
+      </>
+    ),
+  };
 
-  useGlobalKeyboardShortcut('MetaAltArrowUp', event => {
-    event.preventDefault()
-    event.stopPropagation()
-    linkRef.current?.click()
-  })
+  useGlobalKeyboardShortcut('MetaAltArrowUp', (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    linkRef.current?.click();
+  });
 
-  return linkOriginator
-    ? (
-      <button
-        type="button"
-        onClick={() => window.history.back()}
-        {...linkProps}
-      />
-    )
-    : (
-      <OverviewLink
-        to={{}}
-        preventScrollReset={false}
-        {...linkProps}
-      />
-    )
-}
+  return linkOriginator ? (
+    <button
+      type="button"
+      onClick={() => window.history.back()}
+      {...linkProps}
+    />
+  ) : (
+    <OverviewLink to={{}} preventScrollReset={false} {...linkProps} />
+  );
+};
