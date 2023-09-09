6041c37665fa3a40115c3d64b559eddf43d999a0
Joe Anderson <joe@mousetrapped.co.uk>
2023-09-09 14:33:36 +0100

    Do not clean up last blank document per user

--- app/models/clean_up_blank_documents.rb
+++ app/models/clean_up_blank_documents.rb
@@ -1,5 +1,16 @@
 module CleanUpBlankDocuments
   def self.perform
-    Document.blank.where('created_at < ?', 24.hours.ago).destroy_all
+    User.find_each do |user|
+      # Delete all blank documents created more than 24 hours ago except the
+      # most recent one. Leaving the most recent blank document makes it less
+      # likely that the user will return to a deleted blank document.
+      user
+        .documents
+        .blank
+        .where('documents.created_at < ?', 24.hours.ago)
+        .order('documents.created_at DESC')
+        .offset(1)
+        .destroy_all
+    end
   end
 end
