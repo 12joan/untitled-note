d07e3782c20ee017defc628edce8893934aa226e
Joe Anderson <joe@mousetrapped.co.uk>
2024-02-26 10:56:04 +0000

    Restrict relative imports (#361)

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   rules: {
     'prettier/prettier': [
-      1,
+      'error',
       {
         trailingComma: 'es5',
         singleQuote: true,
@@ -70,18 +70,38 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     'react/require-default-props': 'off',
-    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
     'react/jsx-props-no-spreading': 'off',
     'react/no-children-prop': 'off',
     'react/function-component-definition': 'off',
     'react/no-unstable-nested-components': 'off',
     'react/prop-types': 'off',
-    'react/jsx-no-useless-fragment': [1, { allowExpressions: true }],
+    'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
     'jsx-a11y/no-noninteractive-tabindex': 'off',
     'jsx-a11y/anchor-has-content': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
     'jsx-a11y/label-has-associated-control': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
   },
-  overrides: [],
+  overrides: [
+    {
+      files: [
+        // Only direct children of lib and components
+        'client/{lib,components}/*.{ts,tsx}',
+      ],
+      rules: {
+        'no-restricted-imports': [
+          'error',
+          {
+            patterns: [
+              {
+                group: ['.*'],
+                message: 'Relative imports are not allowed.',
+              },
+            ],
+          },
+        ],
+      },
+    }
+  ],
 };
